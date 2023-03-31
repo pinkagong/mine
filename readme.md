af2f0c326cbc71a9981f3bd55638aa9c10bb966e
Lee <rkwk0304@gmail.com>
2023-04-01 01:01:15 +0900

    docker plz Dockerfile 작성

---
+++ readme.md
@@ -0,0 +1,3 @@
+사이트 시작 주소
+
+GET http://localhost:8080/com.solponge/main
--- src/main/java/com/solponge/domain/admin/Delivery.java
+++ src/main/java/com/solponge/domain/admin/Delivery.java
@@ -2,13 +2,19 @@ package com.solponge.domain.admin;
 
 import lombok.Data;
 
+import java.util.Random;
+
+
 @Data
 public class Delivery {
+
     String deliveryInfo;
     Long deliveryNum;
 
     public Delivery() {
     }
+
+
 
     public Delivery(String deliveryInfo, Long deliveryNum) {
         this.deliveryInfo = deliveryInfo;
